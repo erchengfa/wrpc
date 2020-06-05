1ac7c6731bcbed554e7466cc66f3d50d3e799bbf
wang-hello <w1161380236@163.com>
2020-06-05 15:12:05 +0800

    测试类去掉原来的名字suzhe

--- wrpc-example/springboot-demo/springboot-demo-provider/src/main/java/com/github/wang/example/springbootdemo/provider/ExampleProvider.java
+++ wrpc-example/springboot-demo/springboot-demo-provider/src/main/java/com/github/wang/example/springbootdemo/provider/ExampleProvider.java
@@ -3,10 +3,6 @@ package com.github.wang.example.springbootdemo.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author suzhe
- * @date 2018/11/25
- */
 @SpringBootApplication
 public class ExampleProvider {
     public static void main(String[] args) {
