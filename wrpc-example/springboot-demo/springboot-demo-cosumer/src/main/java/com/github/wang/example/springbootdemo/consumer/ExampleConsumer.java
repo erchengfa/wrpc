dce3fcd0806894170769ee7bde410b4921ad75cb
wang-hello <w1161380236@163.com>
2020-06-05 16:06:31 +0800

    我原来的账号suzhe换成新账号wang

--- wrpc-example/springboot-demo/springboot-demo-cosumer/src/main/java/com/github/wang/example/springbootdemo/consumer/ExampleConsumer.java
+++ wrpc-example/springboot-demo/springboot-demo-cosumer/src/main/java/com/github/wang/example/springbootdemo/consumer/ExampleConsumer.java
@@ -3,10 +3,6 @@ package com.github.wang.example.springbootdemo.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author suzhe
- * @date 2018/11/25
- */
 @SpringBootApplication
 public class ExampleConsumer {
     public static void main(String[] args) {
